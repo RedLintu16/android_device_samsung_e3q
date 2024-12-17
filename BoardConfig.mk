6876c86da7e14caad9a70f854a6c59e0c884c67c
David Wronek <david@mainlining.org>
2024-12-17 20:13:31 +0100

    e3q: Prepare fingerprint
    Change-Id: I4458affe2a092c7c636e0b5f147745a74b3390d6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,6 +10,9 @@ BUILD_BROKEN_DUP_RULES := true
 
 DEVICE_PATH := device/samsung/e3q
 
+# A/B
+AB_OTA_UPDATER := false
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv9-a
@@ -256,7 +259,6 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
-# DEVICE_FRAMEWORK_MANIFEST_FILE
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := \
     hardware/qcom-caf/sm8550/audio/primary-hal/configs/common/manifest_non_qmaa.xml \
--- device.mk
+++ device.mk
@@ -94,6 +94,10 @@ PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.1-impl-mock \
     fastbootd
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint-service.samsung
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
